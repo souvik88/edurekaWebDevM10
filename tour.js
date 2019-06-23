7d647613c1e2604d47501519f8ce88cb672bfae8
Souvik Kundu <kivtech88@gmail.com>
2019-06-23 13:54:34 +0530

    commit

---
+++ tour.js
@@ -0,0 +1,16 @@
+var tour = new Tour();
+
+tour.addSteps (
+    [
+        {
+            element: "#col-1",
+            placement: "bottom",
+            title: "Online Live Training",
+            content: "Effective online virtual training best suited for busy professionals like you"
+
+        }
+    ]
+);
+
+tour.init();
+tour.start();
