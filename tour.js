756a786b3176ff057974b01329ea9b04e5bb83fe
Souvik Kundu <kivtech88@gmail.com>
2019-06-23 18:57:41 +0530

    commit

--- tour.js
+++ tour.js
@@ -1,3 +1,4 @@
+// bootstrap tour feature
 var tour = new Tour();
 
 tour.addSteps (
@@ -8,9 +9,36 @@ tour.addSteps (
             title: "Online Live Training",
             content: "Effective online virtual training best suited for busy professionals like you"
 
+        },
+        {
+            element: "#col-2",
+            placement: "bottom",
+            title: "Flexible Timing",
+            content: "Freedom to reschedule your batch at your own convenience"
+        },
+        {
+            element: "#col-3",
+            placement: "bottom",
+            title: "24x7 Helpline Available",
+            content: "Our support team ninjas are just a call away 24 hours a day, 7 days a week"
+        },
+        {
+            orphan: true,
+            title: "Thank you",
+            backdrop: true,
+            placement: "top",
+            content: "Thank you for visiting Edureka"
+
         }
     ]
 );
 
-tour.init();
-tour.start();
+$(document).ready(function() {
+    $('#start-tour-btn').on('click', function() {
+        tour.init();
+        tour.start();
+        tour.restart();
+    });
+
+    
+})
